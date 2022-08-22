b2be704ab4620398db4ed335ddb7a760046dc1fe
OthmanMaher <othmanmaher150@gmail.com>
2022-08-22 19:10:20 +0300

    changes

--- src/game/ChessGame.java
+++ src/game/ChessGame.java
@@ -1,7 +1,12 @@
 package game;
 
+import Squares.Location;
+import Squares.Square;
+import enums.File;
 import player.Player;
 
+import java.util.Arrays;
+import java.util.Map;
 import java.util.Scanner;
 
 public class ChessGame {
@@ -12,22 +17,88 @@ public class ChessGame {
     Scanner scanner = new Scanner(System.in);
 
     public ChessGame(){
+        board=new Board();
         System.out.println("Enter the white player name: ");
         String whiteName = scanner.nextLine();
+
         System.out.println(whiteName);
         System.out.println("Enter the black player name: ");
+        whitePlayer = new Player(whiteName,true);
         String blackName = scanner.nextLine();
+        blackPlayer=new Player(blackName,false);
         System.out.println(blackName);
 
 
     }
-    public void takePlayerInput(){
-        String input = scanner.nextLine();
+    public Square generateSquare(String sq){
+        File file=File.valueOf(String.valueOf(Character.toUpperCase(sq.charAt(0))));
+        int rank = Integer.parseInt(String.valueOf(sq.charAt(1)));
+        return board.getLocationMap().get(new Location(file,rank));
+
+    }
+    public void validateSquare(Square sq){
+                if (!(sq.isOccupied() && isWhiteTurn && sq.getCurrentPiece().isWhite())){
+            throw new IllegalStateException();
+        }
+    }
+    // return array of string and use it
+    public String[] takePlayerInput(){
+
+        String line  = scanner.nextLine();
+        String[] arrOfLines = line.split("\\s+");
+        Map<Location, Square> squareMap = board.getLocationMap();
+        if (arrOfLines.length != 3){
+            throw new IllegalStateException();
+        }
+
+        String mv=arrOfLines[0];
+        String firstMove=arrOfLines[1];
+        String secondMove= arrOfLines[2];
+//        File[] files = File.values();
+//        //check if the input is valid
+//        if (!mv.equals("move")){
+//            throw new IllegalStateException();
+//        }
+//
+        return arrOfLines;
+
+//        File fromFile=File.valueOf(String.valueOf(Character.toUpperCase(firstMove.charAt(0))));
+//        int fromRank = Integer.parseInt(String.valueOf(firstMove.charAt(1)));
+//        File toFile=File.valueOf(String.valueOf(Character.toUpperCase(secondMove.charAt(0))));
+//        int toRank = Integer.parseInt(String.valueOf(secondMove.charAt(1)));
+//         Square fromSq = board.getLocationMap().get(new Location(fromFile,fromRank));
+//         Square toSq = board.getLocationMap().get(new Location(toFile,toRank));
+//
+//        if (!(fromSq.isOccupied() && isWhiteTurn && fromSq.getCurrentPiece().isWhite())){
+//            throw new IllegalStateException();
+//        }
+
+
 
     }
     public void start(){
         board.printBoard();
+        while(true) {
+            String[] inputs = takePlayerInput();
+            String from=inputs[1];
+            String to= inputs[2];
+            isWhiteTurn=!isWhiteTurn;
+            Square fromSq = generateSquare(from);
+            Square toSq= generateSquare(to);
+            //validate squares and the pieces
 
+//            File fromFile=File.valueOf(String.valueOf(Character.toUpperCase(from.charAt(0))));
+//            int fromRank = Integer.parseInt(String.valueOf(from.charAt(1)));
+//            File toFile=File.valueOf(String.valueOf(Character.toUpperCase(to.charAt(0))));
+//            int toRank = Integer.parseInt(String.valueOf(to.charAt(1)));
+//            Square fromSq = board.getLocationMap().get(new Location(fromFile,fromRank));
+//            Square toSq = board.getLocationMap().get(new Location(toFile,toRank));
+//            Square fromSq=new Square(),toSq;
+//            takePlayerInput(fromSq,toSq);
+//            isWhiteTurn = !isWhiteTurn;
+//            board.printBoard();
+
+        }
     }
 }
  /* Board board = new Board();
